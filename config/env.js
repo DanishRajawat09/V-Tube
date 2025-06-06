e5c396d2f6c401bcd9601a6134c9ff225e48d4ac
danishRajawat <ibnfarooq070@gmail.com>
2025-06-06 14:24:16 +0530

    video user model and access and refreash token setup

--- config/env.js
+++ config/env.js
@@ -3,4 +3,4 @@ import dotenv from "dotenv"
 dotenv.config({path : "./.env"})
 
 
- export const {PORT , MONGODB_URI , CORS_ORIGIN} = process.env
+ export const {PORT , MONGODB_URI , CORS_ORIGIN , ACCESS_TOKEN_SECRET ,ACCESS_TOKEN_EXPIRY , REFRESH_TOKEN_SECRET ,REFRESH_TOKEN_EXPIRRY} = process.env
---
+++ src/models/user.models.js
@@ -0,0 +1,97 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import {
+  ACCESS_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRY,
+  REFRESH_TOKEN_SECRET,
+  REFRESH_TOKEN_EXPIRRY,
+} from "../../config/env.js";
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+      minLength: 4,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+      minLength: 4,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 4,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary
+      required: true,
+    },
+    coverimage: {
+      type: String, // cloudinary
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+      minLength: 8,
+    },
+    watchhistory: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    refreshtoken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10);
+
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: REFRESH_TOKEN_EXPIRRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
